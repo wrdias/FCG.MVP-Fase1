42522ae35c86592830c55b938a669ea0040a580d
wrdias <wanderlei.ribeiro@gmail.com>
2025-11-04 22:38:59 -0300

    Configuração de chave.

--- src/FCG.Mvp.API/Program.cs
+++ src/FCG.Mvp.API/Program.cs
@@ -4,6 +4,7 @@ using FCG.Mvp.API.Services;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using Serilog;
 using System;
 using System.Text;
@@ -31,7 +32,36 @@ builder.Services.AddScoped<IGameService, GameService>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "API com Token", Version = "v1" });
+
+    // Define o esquema de segurança "Bearer"
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Name = "Authorization",
+        Description = "Insira seu token JWT aqui. Use 'Bearer ' + seu token.",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.Http,
+        Scheme = "bearer"
+    });
+
+    // Requer autenticação para utilizar o esquema "Bearer"
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            new List<string>()
+        }
+    });
+});
 
 // Fluent Validation
 //builder.Services.AddFluentValidationAutoValidation();
@@ -39,7 +69,7 @@ builder.Services.AddSwaggerGen();
 
 // Authentication (JWT)
 var jwtKey = builder.Configuration["Jwt:Key"];
-var key = Encoding.ASCII.GetBytes(jwtKey ?? "ChangeThisToStrongKeyInProduction123!");
+var key = Encoding.ASCII.GetBytes(jwtKey ?? "I8F1ZUmifA81bscs2LrZk9wZOifzF79FaQHOIGvCvr7GlNgzYk");
 builder.Services.AddAuthentication(options =>
 {
     options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
